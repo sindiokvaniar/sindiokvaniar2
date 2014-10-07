ea54d81e81574a024d9ca0ff4fdef1ef4d3a2aab
selvioktupiana <selvioktupiana@yahoo.com>
2014-10-08 02:39:37 +0700

    no message

---
+++ NetBeansProjects/TugasSatu/src/id/blits/tugas/SindiOkvaniar.java
@@ -0,0 +1,18 @@
+
+package id.blits.tugas;
+
+//PROGRAM INI DIBUAT OLEH SINDI OKVANIAR...
+public class SindiOkvaniar {
+
+    
+    public static void main(String[] args) {
+        String nama = "SINDI OKVANIAR";
+        String npm = "13311361";
+        String kelas = "SI 13 E";
+        System.out.println("Perguruan Tinggi TEKNOKRAT");
+        System.out.println("============================");
+        System.out.println("Nama   : "+nama+"\n"+"NPM    : "+npm+"\n"+"Kelas  : "+kelas);
+     
+    }
+    
+}
